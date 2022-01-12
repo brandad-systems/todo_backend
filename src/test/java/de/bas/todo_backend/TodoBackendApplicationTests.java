75496d2933cfdd004cd579dc19b6627a9c38706b
Bodo Teichmann <bodo.teichmann@brandad-systems.de>
2022-01-12 10:02:12 +0100

    initial commit

---
+++ src/test/java/de/bas/todo_backend/TodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bas.todo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
