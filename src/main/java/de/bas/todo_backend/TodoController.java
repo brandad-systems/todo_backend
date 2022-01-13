757d8fc57f08549eedd137e208f00d4aad4b3c7b
fabian.reichenberg <fabian.reichenberg@outlook.de>
2022-01-13 14:02:03 +0100

    added ResponseEntity

--- src/main/java/de/bas/todo_backend/TodoController.java
+++ src/main/java/de/bas/todo_backend/TodoController.java
@@ -1,5 +1,7 @@
 package de.bas.todo_backend;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +14,14 @@ import java.util.UUID;
 @RequestMapping("/api/v1/todos")
 public class TodoController {
 
-    @GetMapping
-    public List<TodoModel> getTodos() {
+    @GetMapping(produces = "application/json")
+    public ResponseEntity<List<TodoModel>> getTodos() {
         TodoModel temp = new TodoModel(UUID.randomUUID(), "Todo", "Todo erledigen", false);
         TodoModel temp2 = new TodoModel(UUID.randomUUID(), "Todo2", "Todo2 erledigen", false);
 
-        return Arrays.asList(temp, temp2);
+        return new ResponseEntity(Arrays.asList(temp, temp2), HttpStatus.ACCEPTED);
     }
+
+
 
 }
