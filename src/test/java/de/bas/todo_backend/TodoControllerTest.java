57591b4cd54fe597991743449054739544dab95d
Tanja Maier (DATEV) <tanja.maier@brandad-systems.de>
2022-01-17 11:45:19 +0100

    removed unused imports

--- src/test/java/de/bas/todo_backend/TodoControllerTest.java
+++ src/test/java/de/bas/todo_backend/TodoControllerTest.java
@@ -6,14 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
