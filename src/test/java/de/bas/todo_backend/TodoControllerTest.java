01f2bd2f3bd30c41401311085eb754ad1e2868a1
Tanja Maier (DATEV) <tanja.maier@brandad-systems.de>
2022-01-17 11:24:32 +0100

    controller test for get

--- src/test/java/de/bas/todo_backend/TodoControllerTest.java
+++ src/test/java/de/bas/todo_backend/TodoControllerTest.java
@@ -6,8 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = {TodoController.class})
@@ -22,7 +31,16 @@ public class TodoControllerTest {
     private TodoService todoService;
 
     @Test
-    public void validGetReturns200() throws Exception {
-        mockMvc.perform(get("/api/v1/todos")).andExpect(status().isOk());
+    public void validGetReturns200andCallsServiceOnceAndReturnsTodos() throws Exception {
+        //given
+        TodoModel todo1 = new TodoModel(UUID.randomUUID(), "todo1", "test", false);
+        List<TodoModel> todoList = new ArrayList<>(Arrays.asList(todo1));
+        when(todoService.getTodos()).thenReturn(todoList);
+
+        //when
+        mockMvc.perform(get("/api/v1/todos"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(todoList)));
+        verify(todoService, times(1)).getTodos();
     }
 }
