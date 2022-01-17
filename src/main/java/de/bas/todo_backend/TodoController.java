ff3db8fc593917326bed58f71c026ad6147cfe7d
Bodo Teichmann <bodo.teichmann@brandad-systems.de>
2022-01-17 16:36:00 +0100

    @CrossOrigin... added

--- src/main/java/de/bas/todo_backend/TodoController.java
+++ src/main/java/de/bas/todo_backend/TodoController.java
@@ -12,6 +12,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/v1/todos")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class TodoController {
 
     private final TodoService todoService;
